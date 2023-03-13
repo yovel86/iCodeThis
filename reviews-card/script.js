89efdccdd87467dc204f4ba517a7b8b42c9d482c
Yovel <yovel.trichy@gmail.com>
2023-03-13 17:14:57 +0530

    ADD JS in reviews-card

---
+++ reviews-card/script.js
@@ -0,0 +1,32 @@
+const topics = document.querySelectorAll('.topic');
+const stars  = document.querySelectorAll('.rating span');
+
+topics.forEach((topic, i) => {
+  topic.addEventListener('click', () => {
+		topics.forEach(t => t.classList.remove('active'));
+		topic.classList.add('active');
+	});
+});
+
+stars.forEach((star, i) => {
+	star.addEventListener('click', () => {
+		const parentEle = star.parentElement;
+		for(var j = 0; j <= i; j++) {
+				if(stars[j] === parentEle.lastElementChild) {
+					stars[j].textContent = 'star';
+					return;
+				} else {
+					stars[j].textContent = 'star';
+				}
+		}
+		for(let k = j; k < stars.length; k++) {
+			console.log(stars[k])
+			if(stars[k] === parentEle.lastElementChild) {
+				stars[k].textContent = 'star_border';
+				return;
+			} else {
+				stars[k].textContent = 'star_border';
+			}
+		}
+	});
+});
